a7a9552314b70c7d5899a19658f3960d4dbca540
AnaGciaSchz <anags.clm@gmail.com>
2024-10-17 20:11:02 +0200

    Adding spanish Cookie Policy

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import { createWebHistory, createRouter } from 'vue-router';
 
 import HomeView from '@/views/home-view.vue';
 import PrivacyPolicyView from '@/views/privacy-policy-view.vue';
+import CookiePolicyView from '@/views/cookie-policy-view.vue';
 import NotFoundView from '@/views/not-found-view.vue';
 import { ROUTE_NAMES } from '@/router/route-names.const';
 
 const routes = [
   { path: '/', name: ROUTE_NAMES.HOME, component: HomeView },
   { path: '/privacy-policy', name: ROUTE_NAMES.PRIVACY_POLICY, component: PrivacyPolicyView },
+  { path: '/cookie-policy', name: ROUTE_NAMES.COOKIE_POLICY, component: CookiePolicyView },
   { path: '/:pathMatch(.*)*', name: ROUTE_NAMES.NOT_FOUND, component: NotFoundView }
 ];
 
--- src/router/route-names.const.ts
+++ src/router/route-names.const.ts
@@ -1,5 +1,6 @@
 export const ROUTE_NAMES = {
   HOME: 'home',
   PRIVACY_POLICY: 'privacy-policy',
+  COOKIE_POLICY: 'cookie-policy',
   NOT_FOUND: 'not-found'
 };
