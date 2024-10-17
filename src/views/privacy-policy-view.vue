7d0ccab797a384177e25b9cb96328f1a4f854564
AnaGciaSchz <anags.clm@gmail.com>
2024-10-17 19:55:08 +0200

    Referencing Agenda Gijon

--- src/views/privacy-policy-view.vue
+++ src/views/privacy-policy-view.vue
@@ -2,13 +2,37 @@
   <h1>Política de Privacidad</h1>
   <p>Última actualización: 17 Octubre 2024</p>
   <p>
-    Este sitio web no recopila ni almacena ninguna información personal de los usuarios. 
-    No solicitamos ni procesamos datos de identificación, direcciones de correo electrónico, ubicaciones, 
-    historial de navegación u otra información personal.
+    Este sitio web (o aplicación) recopila y utiliza información proporcionada por terceros, en particular, 
+    datos obtenidos de la API de <a href= "https://www.gijon.es/es/eventos">Agenda Gijón</a>. A continuación, describimos cómo gestionamos esta información 
+    y cómo se protege tu privacidad.
+  </p>
+  <h2>Recopilación de información</h2>
+  <p>
+    Este sitio web no recopila información personal de los usuarios, como nombres, 
+    direcciones de correo electrónico o información de identificación personal. Sin embargo, agregamos 
+    y mostramos información de eventos, actividades y otros datos públicos proporcionados por terceros a 
+    través de APIs y otros servicios.
+  </p>
+  <h2>Uso de la API de Agenda Gijón</h2>
+  <p>
+    Este sitio web utiliza la API de Agenda Gijón para agregar información pública sobre eventos, actividades y 
+    servicios ofrecidos en la ciudad de Gijón. Estos datos incluyen:
+    <ul>
+      <li>Nombres de eventos</li>
+      <li>Fechas y horarios</li>
+      <li>Localizaciones</li>
+      <li>Descripciones de actividades</li>
+    </ul>
+    Toda la información obtenida de la API de Agenda Gijón es pública y no incluye datos personales. 
+    Para más detalles sobre la forma en que se gestionan los datos de la Agenda Gijón, te recomendamos 
+    revisar la  <a href= "https://www.gijon.es/es/privacidad">Política de Privacidad de Agenda Gijón</a>.
   </p>
   <h2>Cookies y tecnologías similares</h2>
   <p>
-    Este sitio web no utiliza cookies ni tecnologías de seguimiento para recopilar información sobre sus visitas.
+    Este sitio web no utiliza cookies ni tecnologías de seguimiento para recopilar información personal o 
+    comportamientos de los usuarios. Sin embargo, algunos de los servicios de terceros que utilizamos, 
+    como la API de <a href= "https://www.gijon.es/es/eventos">Agenda Gijón</a>, podrían recopilar datos no identificables sobre el uso de sus plataformas. 
+    Te recomendamos revisar sus políticas de privacidad para entender mejor su gestión de datos.
   </p>
   <h2>Enlaces a sitios web de terceros</h2>
   <p>Nuestro sitio puede contener enlaces a otros sitios web. Si haces clic en un enlace de terceros, serás 
@@ -21,7 +45,7 @@
     Nos reservamos el derecho de actualizar esta política de privacidad en cualquier momento. 
     Te recomendamos revisar esta página periódicamente para estar al tanto de cualquier cambio.
   </p>
-  <h2>Contáctanos</h2>
+  <h2>Contacto</h2>
   <p>Si tienes alguna duda sobre esta política de privacidad, puedes contactarnos por e-mail: <a href="mailto:distributed.pixels.project@gmail.com">distributed.pixels.project@gmail.com</a>
   </p>
 </template>
